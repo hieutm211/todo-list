5d0ac2f09da9ddbc2bf4198ac9cfc2c300fdca95
hieutran <hieutm211@gmail.com>
2020-07-29 15:07:38 +0700

    ver1.0 implement HTML structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+function Header(props) {
   return (
-	<div>This is starter page</div>  
+    <header>
+        <nav>
+          <span>
+            this is date
+          </span>
+          <span>
+            <button>incomplete tasks</button>
+            <button>completed tasks</button>
+          </span>
+        </nav>
+        
+        <span>active tasks</span>
+    </header>
+  );
+}
+
+function Form(props) {
+  return (
+    <div>
+        <input></input>
+        <button>add task</button>  
+        <div>error message</div>
+    </div>
+  );
+}
+
+function List(props) {
+  return (
+    <ul>
+      {props.value.map((el) => {
+        return (
+          <li key={el.id}>
+            <button>tick</button>
+            {el.description}
+            <button>delete</button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+function App() {
+  const [incompleteList, setIncompleteList] = useState([]);
+  const [completedList, setCompletedList] = useState([]);
+  const [inIncompletedList, setInIncompletedList] = useState(true);
+  const [currentList, setCurrentList] = useState(incompleteList);
+
+  return (
+    <div class="App">
+      <Header />
+      <Form />
+      <List value={currentList}/>
+    </div>
   );
 }
 
