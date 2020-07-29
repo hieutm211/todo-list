c0183cb8b550858277367e65810d0ba55b84fbea
hieutran <hieutm211@gmail.com>
2020-07-29 16:22:45 +0700

    add 'move task between incomplete/completed tasks' feature

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ function Header(props) {
             this is date
           </span>
           <span>
-            <button>incomplete tasks</button>
-            <button>completed tasks</button>
+            <button onClick={ () => props.setCurrentList(0) }>incomplete tasks</button>
+            <button onClick={ () => props.setCurrentList(1) } >completed tasks</button>
           </span>
         </nav>
         
-        <span>active tasks</span>
+      <span>{props.activeTask} active tasks</span>
     </header>
   );
 }
@@ -46,9 +46,9 @@ function List(props) {
       {props.value.map((el) => {
         return (
           <li key={el.id}>
-            <button>tick</button>
+            <button onClick={() => props.moveTask(el.id) }>tick</button>
             {el.description}
-            <button onClick={() => props.removeTask(el.id)}>delete</button>
+            <button onClick={() => props.removeTask(el.id) }>delete</button>
           </li>
         );
       })}
@@ -59,9 +59,12 @@ function List(props) {
 function App() {
   const [incompleteList, setIncompleteList] = useState([]);
   const [completedList, setCompletedList] = useState([]);
-  const [inIncompletedList, setInIncompletedList] = useState(true);
   const [currentList, setCurrentList] = useState(0);
   const [currentId, setCurrentId] = useState(0);
+
+  function getCurrentList() {
+    return currentList ? completedList : incompleteList;
+  }
 
   function addTask(description) {
     setIncompleteList([...incompleteList, {id: currentId, description}]);
@@ -69,7 +72,7 @@ function App() {
   }
 
   function removeTask(id) {
-    let list = currentList ? completedList : incompleteList;
+    let list = getCurrentList();
     let newList = [];
     
     for (let el of list) {
@@ -83,13 +86,35 @@ function App() {
     } else {
       setCompletedList(newList);
     }
-  } 
+  }
+
+  function moveTask(id) {
+    let list = getCurrentList();
+    
+    //get value of task (whose id = id)
+    let task;
+    for (let el of list) {
+      if (el.id === id) {
+        task = {...el};
+      }
+    }
+
+    //remove it from current list
+    removeTask(id);
+
+    //add it to the other list
+     if (currentList === 0) {
+       setCompletedList([...completedList, task]);
+    } else {
+       setIncompleteList([...incompleteList, task]);
+    }
+  }
 
   return (
     <div class="App">
-      <Header />
+      <Header activeTask={incompleteList.length} setCurrentList={setCurrentList}/>
       <Form addTask={addTask}/>
-      <List value={currentList ? completedList : incompleteList} removeTask={removeTask}/>
+      <List value={getCurrentList()} moveTask={moveTask} removeTask={removeTask}/>
     </div>
   );
 }
